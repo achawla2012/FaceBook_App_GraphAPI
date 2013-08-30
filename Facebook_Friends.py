79208855d87b928099fdfa3a285f0e8774830f2a
achawla2012 <chawl007@umn.edu>
2013-08-29 20:54:09 -0500

    Added module to post message on the group's wall.

--- Facebook_Friends.py
+++ Facebook_Friends.py
@@ -1,6 +1,9 @@
 import Facebook
 import os
 
+def PostOnGroupWall(GroupID):
+    facebook.put_object(GroupID, "feed", message="<MESSAGE>")    
+
 def facebook_friend():    
     facebook = Facebook.GraphAPI('<ACCESS_TOKEN>')
     user = facebook.get_object("me")
@@ -8,6 +11,6 @@ def facebook_friend():
     for friend in friends['data']:
         print friend['name'].encode('utf-8')
 
-
 if __name__=="__main__":
     facebook_friend()
+    PostOnGroupWall("<GROUP_ID>")
